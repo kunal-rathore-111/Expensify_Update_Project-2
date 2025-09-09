57ffc2ed97503ce91dc3ab840f3af544f2b57427
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-09-10 02:40:57 +0530

    redefining frontend + did some functionality like the useContext api for having the state in child elements and useFetch custom hook for fetching

--- client/c/src/Login/googleLogin.jsx
+++ client/c/src/Login/googleLogin.jsx
@@ -1,36 +1,28 @@
-
-
 // google login page for Our website
 
-import "./googleLogin.css";
-import accountImage from "../assets/accountImg.png"
+import './googleLogin.css';
+import accountImage from '../assets/accountImg.png';
 
 export const GoogleLogin = () => {
+  return (
+    <div className="GoogleLoginPage page">
+      <div className="herodiv">
+        <h1 className="welcomeMessage">Welcome to Expensify</h1>
+        <br />
+        <br />
 
-    return <div className="GoogleLoginPage page">
-
-        <div className="herodiv">
-
-            <h1 className="welcomeMessage">Welcome to Expensify</h1>
-            <br /><br />
-
-            <div className="login_with_googleDiv">
-
-
-                <li className="googleLink_and_googleLogo">
-
-                    {/* <!-- need to change for development --> */}
-                    <a href="http://localhost:3000/api/user/auth/google">
-
-                        <div className="googleLogoDiv">
-                            <img src={accountImage} className="googleLogoImage" />
-                        </div>
-                        Login with Google Account
-                    </a>
-                </li>
-            </div>
+        <div className="login_with_googleDiv">
+          <li className="googleLink_and_googleLogo">
+            {/* <!-- need to change for development --> */}
+            <a href="http://localhost:3000/api/user/auth/google">
+              <div className="googleLogoDiv">
+                <img src={accountImage} className="googleLogoImage" />
+              </div>
+              Login with Google Account
+            </a>
+          </li>
         </div>
+      </div>
     </div>
-
-}
-
+  );
+};
