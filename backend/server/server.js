4667eaf024c23e9498c409468d55f848f50e0af1
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-10-24 15:48:16 +0530

    cors update to *

--- backend/server/server.js
+++ backend/server/server.js
@@ -14,7 +14,7 @@ connectDb();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true, }));
+app.use(cors({ origin: "*", credentials: true, }));
 //session
 app.use(session({
     secret: process.env.SESSION_SECRET,
